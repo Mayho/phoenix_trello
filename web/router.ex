2bc00b55421cd3f1c46dadf524368249564f341c
Mehul Patel <mehul8@gmail.com>
2016-07-11 23:16:45 -0700

    feat(app): wip add reducters, routes, store, root container

--- web/router.ex
+++ web/router.ex
@@ -13,14 +13,17 @@ defmodule PhoenixTrello.Router do
     plug :accepts, ["json"]
   end
 
+  scope "/api", PhoenixTrello do
+    pipe_through :api
+
+    scope "/v1" do
+      post "/registrations", RegistrationController, :create
+    end
+  end
+  
   scope "/", PhoenixTrello do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "*path", PageController, :index
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", PhoenixTrello do
-  #   pipe_through :api
-  # end
 end
